bcb610a1df633dddf609cb85c24bc59b0fa375cb
spiCkyyy <hassam_waheed@outlook.com>
2023-11-11 23:36:10 +0000

    Axios implemented, my first call from client to server

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld/>
+  <!-- <HelloWorld msg="Hello Lameya, Bye."/> -->
 </template>
 
 <script>
